38f8f4a9aa5626f41376e7da5e8f9eb596c73d78
YourContender <dream-boy123@ukr.net>
2024-12-18 20:12:28 +0200

    fix mistake

--- src/helpers/custom-design/marquee/Marquee.jsx
+++ src/helpers/custom-design/marquee/Marquee.jsx
@@ -6,10 +6,10 @@ export const Marquee = ({ cost, marquee }) => {
 		<div className="marquee">
 			<div className="marquee-inner run">
 				<div className="marquee-inner-item">
-					ЗНИЖКА 49% ВСТИГНИ КУПИТИ ВСЬОГО ЗА <span>1099</span> ГРН !
+					ВСТИГНИ КУПИТИ ВСЬОГО ЗА <span>1099</span> ГРН !
 				</div>
 				<div className="marquee-inner-item">
-					ЗНИЖКА 49% ВСТИГНИ КУПИТИ ВСЬОГО ЗА <span>1099</span>  ГРН !
+					ВСТИГНИ КУПИТИ ВСЬОГО ЗА <span>1099</span>  ГРН !
 				</div>
 			</div>
 		</div>
