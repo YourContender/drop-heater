e071a2c2f8e8acb01289bdb6754c6df85b8a48dd
YourContender <dream-boy123@ukr.net>
2025-03-04 20:10:58 +0200

    create details section for card product

--- src/helpers/custom-drop/thanks-page/ThanksPage.jsx
+++ src/helpers/custom-drop/thanks-page/ThanksPage.jsx
@@ -1,8 +1,9 @@
-import { Slider } from "../../slider/Slider";
+// import { Slider } from "../../slider/Slider";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import "./ThanksPage.scss";
 import { useState } from "react";
+import { Card } from "./thanks-card/Card";
 
 export const ThanksPage = ({ 
     thanksPage, 
@@ -17,7 +18,7 @@ export const ThanksPage = ({
 
     if (!isOpen) return null;
 
-    const testFunc = (title, newPrice) => {
+    const sendRequestUpsell = (title, newPrice) => {
         sendUpsell(title, newPrice);
         setOpenModalTnk(true);
     }
@@ -67,31 +68,17 @@ export const ThanksPage = ({
                 </div>
                 <div className="modal_market-list">
                     {thanksPage.map((item, index) => (
-                        <div className="modal_market-list-card" key={index}>
-                            <h3 className="card-title">{item.title}</h3>
-                            <Slider data={item.images} />
-
-                            <div className="card-poster">
-                                <div className="card-poster-text">
-                                    <span>
-                                        {item.saleMessage}
-                                        <span className="card-poster-text-percent"> {item.percent}</span>
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div className="card-price">
-                                <div className="card-price-old">{item.old} грн</div>
-                                <div className="card-price-new">{item.new} грн</div>
-                            </div>
-
-                            <button className="card-btn" onClick={(e) => {
-                                // e.stopPropagation();
-                                testFunc(item.title, item.new);
-                            }}>
-                                Замовити
-                            </button>
-                        </div>
+                        <Card 
+                            key={index}
+                            images={item.images} 
+                            title={item.title}   
+                            sale={item.saleMessage}
+                            percent={item.percent}
+                            oldPrice={item.old}
+                            newPrice={item.new}
+                            sendRequestUpsell={sendRequestUpsell}
+                            details={item.details}
+                        />
                     ))}
                 </div>
             </div>
---
+++ src/helpers/custom-drop/thanks-page/thanks-card/Card.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { Slider } from '../../../slider/Slider';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import "../ThanksPage.scss"
+
+export const Card = ({ 
+    images, 
+    title, 
+    sale, 
+    percent, 
+    oldPrice, 
+    newPrice, 
+    details,
+    sendRequestUpsell
+}) => {
+    const [isOpenOther, setIsOpenOther] = useState(false);
+
+    const product = {
+        name: "Ноутбук XYZ",
+        // details: ["Процессор: Intel i7", "ОЗУ: 16GB", "SSD: 512GB"],
+        details: [
+            {
+                t: "Процессор:",
+                d: "Intel i7"
+            },
+            {
+                t: "ОЗУ:",
+                d: "16GB"
+            },
+            {
+                t: "SSD:",
+                d: "512GB"
+            },
+        ],
+    };
+
+    return (
+        <div className="modal_market-list-card">
+            <h3 className="card-title">{title}</h3>
+            <Slider data={images} />
+
+            <div className="card-poster">
+                <div className="card-poster-text">
+                    <span>
+                        {sale}
+                        <span className="card-poster-text-percent"> {percent}</span>
+                    </span>
+                </div>
+            </div>
+
+            <button className="toggle-btn" onClick={() => setIsOpenOther(!isOpenOther)}>
+                Детальніше {isOpenOther ? 
+                    <FontAwesomeIcon className="arrow-icon" icon={faAngleUp} /> 
+                    : 
+                    <FontAwesomeIcon className="arrow-icon" icon={faAngleDown}/>
+                }
+            </button>
+            <div className={`thanks-details ${isOpenOther ? "open" : ""}`}>
+                <ul>
+                    {details.map((detail, index) => (
+                        <li key={index}>
+                            <span className="thanks-details-title">{detail.t}</span>
+                            <span className="thanks-details-descr">{detail.d}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="card-price">
+                <div className="card-price-old">{oldPrice} грн</div>
+                <div className="card-price-new">{newPrice} грн</div>
+            </div>
+
+            <button className="card-btn" onClick={(e) => {
+                // e.stopPropagation();
+                sendRequestUpsell(title, newPrice);
+            }}>
+                Замовити
+            </button>
+        </div>
+    )
+}
